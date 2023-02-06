c58fc1496faf46f06788f3b11042979188fb74ad
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-02-06 09:19:24 +0100

    fixes3

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -14,44 +14,75 @@ const Sidebar = () => {
   const appContext = useContext(AppContext) as {favoriteIngredients:string[],favoriteMeals:string[],addFavoriteMeal:(arg:string)=>{},addFavoriteIngredient:(arg:string)=>{}};
   const {favoriteIngredients,favoriteMeals,addFavoriteMeal,addFavoriteIngredient} = appContext;
 
-  const ingredients = ["Flout","Milk","Eggs","Beef","Onions"]
-  return (
-    <div className='sidebar'>
-      <SidebarSection name={"Meal"} data={favoriteMeals} showDelete={true}/> 
-      <SidebarSection name={"Ingredient"} data={ingredients} showDelete={false}/>
-    </div>
-  )
-}
 
+  const [showSidebar, setShowSidebar] = useState(true);
 
+  const toggleSidebar = ()=>{
+    setShowSidebar(showSidebar=>!showSidebar);
+  }
 
-
-const SidebarSection = ({name,data,showDelete}:SidebarSection) => {
+  const SidebarSection = ({name,data,showDelete}:SidebarSection) => {
 
   
   
-  const appContext = useContext(AppContext) as {favoriteIngredients:string[],favoriteMeals:string[],addFavoriteMeal:(arg:string)=>{},addFavoriteIngredient:(arg:string)=>{},deleteMeal:(arg:string)=>{}};
-  const {deleteMeal} = appContext;
-  
-  return (
-    <div className='sidebarSection'>
-      <p className="sectionName">{name}</p>
-      <div className="sectionItems">
-      {
-        data.map((item,id)=>{
-          
-          return (
-            <div className="itemWrap" key={id}>
-              <p key={id} className="item">{item}</p>
-              {showDelete && <button onClick={()=>{deleteMeal(item)}} className='deleteItem'>X</button>}
-            </div>
-          )
-        })
-      }
+    const appContext = useContext(AppContext) as {favoriteIngredients:string[],favoriteMeals:string[],addFavoriteMeal:(arg:string)=>{},addFavoriteIngredient:(arg:string)=>{},deleteMeal:(arg:string)=>{}};
+    const {deleteMeal} = appContext;
+    
+    return (
+      <div className={`sidebarSection${showSidebar?'':'Hidden'}`}>
+        <p className="sectionName">{name}</p>
+        <div className="sectionItems">
+        {
+          data.map((item,id)=>{
+            
+            return (
+              <div className="itemWrap" key={id}>
+                <p key={id} className="item">{item}</p>
+                {showDelete && <button onClick={()=>{deleteMeal(item)}} className='deleteItem'>X</button>}
+              </div>
+            )
+          })
+        }
+        </div>
       </div>
+    )
+  }
+
+
+
+
+  const ingredients:string[] = [];
+  return (
+    <div className={`sidebar${showSidebar?'':'Hidden'}`}>
+      <div className="toggleDiv">
+        <button className='toggleDivButton' onClick={toggleSidebar}><b>{showSidebar?'<-':'->'}</b></button>
+      </div>
+      <SidebarSection name={"Meal"} data={favoriteMeals} showDelete={true}/> 
+      <SidebarSection name={"Ingredient"} data={favoriteIngredients} showDelete={false}/>
     </div>
   )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 }
+
+
+
+
+
 
 
 
